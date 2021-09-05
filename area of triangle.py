7eecf3a4026170391a3723b9657f1ca24f03e17d
AKHLAK ANSARI <87934721+AkhlakAnsari@users.noreply.github.com>
2021-09-06 00:39:40 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,14 @@
+#area of triangle
+#written by ER AKHLAK ANSARI
+a= float(input("BASE OF TRIANGLE:"))
+b=float(input("HEIGHT OF TRIANGLE:"))
+#AREA = BASE*HEIGHT/2
+area=a*b/2
+#upto to decimal point result value
+print(" AREA OF TRIANGLE  = %.2f " % area)
+#thanks
+#akhlakansari94@gmail.com
+
+
+
+
